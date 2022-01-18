813361e2e8cde8be1c1c66a55116f03c2c77d19e
brian <brian.rook@gmail.com>
2022-01-17 17:20:01 -0700

    new component test

--- src/main/java/com/bullyrooks/cloud_application/controller/dto/CreateMessageRequestDTO.java
+++ src/main/java/com/bullyrooks/cloud_application/controller/dto/CreateMessageRequestDTO.java
@@ -1,8 +1,14 @@
 package com.bullyrooks.cloud_application.controller.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateMessageRequestDTO {
     private String firstName;
     private String lastName;
--- src/main/java/com/bullyrooks/cloud_application/controller/dto/CreateMessageResponseDTO.java
+++ src/main/java/com/bullyrooks/cloud_application/controller/dto/CreateMessageResponseDTO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CreateMessageResponseDTO {
+    private String messageId;
     private String firstName;
     private String lastName;
     private String message;
