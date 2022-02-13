d4374ec44ceb3fbba2423d40cfea5bcf45fbf075
brian <brian.rook@gmail.com>
2022-02-13 14:52:23 -0700

    message generator health

--- src/main/java/com/bullyrooks/cloud_application/config/MessageGeneratorHealth.java
+++ src/main/java/com/bullyrooks/cloud_application/config/MessageGeneratorHealth.java
@@ -1,23 +1,33 @@
 package com.bullyrooks.cloud_application.config;
 
 import com.bullyrooks.cloud_application.message_generator.client.MessageGeneratorClient;
+import com.bullyrooks.cloud_application.message_generator.client.MessageGeneratorHealthClient;
 import com.bullyrooks.cloud_application.message_generator.client.dto.HealthCheckDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MessageGeneratorHealth implements HealthIndicator {
 
     @Autowired
-    MessageGeneratorClient messageClient;
+    MessageGeneratorHealthClient messageClient;
 
     public Health health() {
         Health.Builder status = Health.up();
-        HealthCheckDTO healthCheckDTO = messageClient.getHealth();
-        if (!StringUtils.equals("UP", healthCheckDTO.getStatus())) {
+        try {
+            ResponseEntity<HealthCheckDTO> healthCheckDTO = messageClient.getHealth();
+            log.info("health check response: {}\n{}", healthCheckDTO.getStatusCode(), healthCheckDTO.getBody());
+            if (!StringUtils.equals("UP", healthCheckDTO.getBody().getStatus())) {
+                status = Health.down();
+            }
+        }catch (Exception e){
+            log.error("error trying to get message generator health: {}", e.getMessage(),e);
             status = Health.down();
         }
         return status.build();
--- src/main/java/com/bullyrooks/cloud_application/message_generator/client/MessageGeneratorHealthClient.java
+++ src/main/java/com/bullyrooks/cloud_application/message_generator/client/MessageGeneratorHealthClient.java
@@ -2,17 +2,17 @@ package com.bullyrooks.cloud_application.message_generator.client;
 
 import com.bullyrooks.cloud_application.message_generator.client.dto.HealthCheckDTO;
 import com.bullyrooks.cloud_application.message_generator.client.dto.MessageResponseDTO;
-import feign.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "message-generator")
-public interface MessageGeneratorClient {
-    @GetMapping(value = "/message",
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    MessageResponseDTO getMessage();
+@FeignClient(contextId = "healthClient", name = "message-generator")
+public interface MessageGeneratorHealthClient {
 
+    @GetMapping(value = "/actuator/health",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    ResponseEntity<HealthCheckDTO> getHealth();
 }
